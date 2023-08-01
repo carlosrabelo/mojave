e8271bdc210e793f45771d659d0309d438f1d050
Carlos Rabelo <developer@carlosrabelo.com.br>
2023-07-31 21:18:00 -0400

    feat: plug trs80m1l2 into SDL and Qt6 screens

--- TODO.md
+++ TODO.md
@@ -137,7 +137,7 @@ Reuse `devices/trs80m1/`; Level II-only expansion stays under `devices/trs80m1l2
 - [x] Keyboard matrix device (reuse Model I family)
 - [x] Wire keyboard matrix to SDL/Qt6 host input
 - [x] Machine clock 1.774 MHz
-- [ ] Plug into SDL/Qt6 virtual screen
+- [x] Plug into SDL/Qt6 virtual screen
 - [ ] Host layout keyboard typing (SDL/Qt6)
 - [ ] Expansion RAM map (32 KiB at 0x8000)
 - [ ] Expansion interface port decode (E8h–EFh printer and serial idle stub)
---
+++ mojave/tests/machines/trs80m1l2/test_virtual_screen.cpp
@@ -0,0 +1,76 @@
+#include <cstdint>
+#include "catch.hpp"
+#include "machines/trs80m1l2/trs80m1l2_preset.hpp"
+#include "machines/shared/builtin_preset_registry.hpp"
+#include "machines/shared/machine.hpp"
+#include "devices/shared/framebuffer.hpp"
+#include "devices/shared/screen_registry.hpp"
+#include "devices/trs80m1/video_controller.hpp"
+#include "session/runner.hpp"
+#include "session/clock.hpp"
+#include "cpus/z80.hpp"
+
+namespace {
+
+Framebuffer* findMachineFramebuffer(Machine& machine) {
+    for (const auto& dev : machine.ownedDevices()) {
+        if (auto* fb = dynamic_cast<Framebuffer*>(dev.get()))
+            return fb;
+    }
+    return nullptr;
+}
+
+} // namespace
+
+TEST_CASE("TRS-80 Model I Level II preset is eligible for SDL and Qt6 frontends",
+          "[machine][trs80m1l2][integration]") {
+    const BuiltinPresetDescriptor* preset = findBuiltinPreset("trs80m1l2");
+    REQUIRE(preset != nullptr);
+    REQUIRE(preset->needs_virtual_screen);
+
+    auto machine = createBuiltinMachine("trs80m1l2");
+    REQUIRE(machine != nullptr);
+
+    Framebuffer* fb = findMachineFramebuffer(*machine);
+    REQUIRE(fb != nullptr);
+    REQUIRE(fb->width() == Trs80M1VideoController::kFramebufferWidth);
+    REQUIRE(fb->height() == Trs80M1VideoController::kFramebufferHeight);
+}
+
+TEST_CASE("TRS-80 Model I Level II virtual screen shows VRAM through the screen registry",
+          "[machine][trs80m1l2][integration]") {
+    auto machine = createTrs80M1L2Machine();
+    Framebuffer* fb = findMachineFramebuffer(*machine);
+    REQUIRE(fb != nullptr);
+
+    ScreenRegistry registry;
+    registry.registerScreen("main", *fb);
+
+    runner::boot(*machine);
+    REQUIRE(fb->getPixel(0, 0) == 0xFF000000u);
+
+    machine->bus().write(Trs80M1L2PresetContract::vram_start, 'A');
+    machine->bus().write(Trs80M1L2PresetContract::vram_start + 1, 'B');
+
+    Framebuffer* screen = registry.screen("main");
+    REQUIRE(screen == fb);
+    REQUIRE(screen->getPixel(2, 2) == 0xFFFFFFFFu);
+    REQUIRE(screen->getPixel(8, 2) == 0xFFFFFFFFu);
+}
+
+TEST_CASE("TRS-80 Model I Level II frontend frame loop uses guest clock pacing",
+          "[machine][trs80m1l2][integration]") {
+    auto machine = createTrs80M1L2Machine();
+    const BuiltinPresetDescriptor* preset = findBuiltinPreset("trs80m1l2");
+    REQUIRE(preset != nullptr);
+
+    Clock clock(preset->guest_cpu_clock_hz);
+    runner::boot(*machine);
+
+    machine->bus().write(Trs80M1L2PresetContract::ram_start, 0x18);
+    machine->bus().write(Trs80M1L2PresetContract::ram_start + 1, 0xFE);
+    dynamic_cast<Z80&>(machine->cpu()).regs().pc = Trs80M1L2PresetContract::ram_start;
+
+    const unsigned cycles = runner::stepForFrame(*machine, clock);
+    REQUIRE(cycles == clock.cyclesPerFrame());
+}
